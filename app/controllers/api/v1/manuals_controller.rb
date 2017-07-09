55327150d4f282de5edc36fa9ed4a3639debeae9
Theron Humiston <theronhumiston@gmail.com>
2017-07-09 18:10:22 -0500

    remove carrierwave magic

--- app/controllers/api/v1/manuals_controller.rb
+++ app/controllers/api/v1/manuals_controller.rb
@@ -23,20 +23,4 @@ class Api::V1::ManualsController < ApplicationController
     manual_params.merge!(parent_params) if params[:parent_id]
     manual_params
   end
-
-  def create_params
-    params.required(:manual)
-  end
-
-  def convert_to_upload(file)
-    temp_file = file.tempfile
-    temp_file.binmode
-    temp_file.rewind
-
-    ActionDispatch::Http::Uploadedfirmware.new({
-      manualname: manual.original_filename,
-      type: file.content_type,
-      tempmanual: temp_file
-    })
-  end
 end
